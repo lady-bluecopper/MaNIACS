2ddb28abf01bce3e4246ca86f8654b7be01ca368
YihuaWei <yihuawei@usc.edu>
2021-06-25 16:48:30 +0800

    add customized run script

---
+++ run_.sh
@@ -0,0 +1,43 @@
+JVM='java -Xmx200g -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewSize=6g -XX:+CMSParallelRemarkEnabled -XX:+ParallelRefProcEnabled -XX:+CMSClassUnloadingEnabled -cp'
+MANIAC_jar='./MANIAC/target/MANIAC-1.0.jar:./MANIAC/target/MaNIAC-1.0-jar-with-dependencies.jar anonymous.maniac.Main'
+lattice_generation_jar='./MANIAC/target/MANIAC-1.0.jar:./MANIAC/target/MaNIAC-1.0-jar-with-dependencies.jar anonymous.maniac.lattice.LatticeGeneration'
+
+input_data='./datasets/'
+output_data='./output/'
+dataset=citeseer
+
+patternSize=3
+seed=11
+failure=0.1
+c=0.5
+percent=false
+
+frequency=0.19
+sampleSize=1700
+numLabels=6
+preComputed=false
+isExact=false
+
+
+OUTPUT="$input_data/lattices/"
+mkdir -p $OUTPUT
+$JVM $lattice_generation_jar \
+	dataFolder=${input_data} \
+	patternSize=${patternSize} \
+	numLabels=${numLabels} \
+
+
+$JVM $MANIAC_jar \
+	dataFolder=${input_data} \
+	outputFolder=${output_data} \
+	inputFile=${dataset}.lg \
+	patternSize=$patternSize \
+	seed=$seed \
+	failure=$failure \
+	c=$c \
+	percent=$percent \
+	frequency=${frequency} \
+	sampleSize=${sampleSize} \
+	numLabels=${numLabels} \
+	preComputed=${preComputed} \
+	isExact=${isExact} 
